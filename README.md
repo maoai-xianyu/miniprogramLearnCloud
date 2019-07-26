dce8f56e74d1732a8cd56ea30b65ee717620e121
zhangkun@boxfish.cn <194264514@qq.com>
2019-07-26 11:22:32 +0800

    添加数据

--- README.md
+++ README.md
@@ -140,9 +140,15 @@ App({
 
 ## 136~140 数据库使用
 
-### 创建数据库，初始化数据
+### 创建数据库，初始化数据，查询数据
 ```
 // dbdemo/pages/index/index.js
+// 操作数据库
+// const db = wx.cloud.database({
+//     env: "codingtk-dev-9nkey",
+// })
+const db = wx.cloud.database(); // 不添加 env 默认是当前环境
+
 Page({
 
   /**
@@ -156,11 +162,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    // 操作数据库
-    // const db = wx.cloud.database({
-    //     env: "codingtk-dev-9nkey",
-    // })
-    const db = wx.cloud.database(); // 不添加 env 默认是当前环境
+    this.selectActicle();
+
+  },
+  // 获取数据
+  selectActicle: function() {
     // 一般写法
     db.collection('article').get({
       success: res => {
@@ -171,7 +177,48 @@ Page({
     db.collection('article').get().then(res => {
       console.log(res);
     });
-    
+  },
+})
+```
+### 添加数据
+```
+// dbdemo/pages/index/index.js
+// 操作数据库
+// const db = wx.cloud.database({
+//     env: "codingtk-dev-9nkey",
+// })
+const db = wx.cloud.database(); // 不添加 env 默认是当前环境
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.addActicle();
+  },
+
+  // 添加数据
+  addActicle: function() {
+    db.collection('article').add({
+      data:{
+        title:"我国单身人口规模已达2.4亿，你以为单身只是影响了父母抱孙子？",
+        author:"三联生活周刊",
+        pub_date:new Date(),
+        content: "靠外卖吃饭，住小而美的居所，囤购喜欢的物品，通过网上交友相亲，从旅行健身取乐，与宠物为伴，遵从自己内心的需求，敢于提前透支消费能力，单身群体的生活状态折射着社会发展和生活方式的变化。",
+        tags:["国际","科技"]
+      }
+    }).then(res =>{
+      console.log("添加成功");
+      console.log(res);
+    })
   }
 })
 ```
--- dbdemo/pages/index/index.js
+++ dbdemo/pages/index/index.js
@@ -1,4 +1,10 @@
 // dbdemo/pages/index/index.js
+// 操作数据库
+// const db = wx.cloud.database({
+//     env: "codingtk-dev-9nkey",
+// })
+const db = wx.cloud.database(); // 不添加 env 默认是当前环境
+
 Page({
 
   /**
@@ -12,11 +18,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    // 操作数据库
-    // const db = wx.cloud.database({
-    //     env: "codingtk-dev-9nkey",
-    // })
-    const db = wx.cloud.database(); // 不添加 env 默认是当前环境
+    this.selectActicle();
+    //this.addActicle();
+  },
+  // 获取数据
+  selectActicle: function() {
     // 一般写法
     db.collection('article').get({
       success: res => {
@@ -27,55 +33,20 @@ Page({
     db.collection('article').get().then(res => {
       console.log(res);
     });
-    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  // 添加数据
+  addActicle: function() {
+    db.collection('article').add({
+      data:{
+        title:"我国单身人口规模已达2.4亿，你以为单身只是影响了父母抱孙子？",
+        author:"三联生活周刊",
+        pub_date:new Date(),
+        content: "靠外卖吃饭，住小而美的居所，囤购喜欢的物品，通过网上交友相亲，从旅行健身取乐，与宠物为伴，遵从自己内心的需求，敢于提前透支消费能力，单身群体的生活状态折射着社会发展和生活方式的变化。",
+        tags:["国际","科技"]
+      }
+    }).then(res =>{
+      console.log("添加成功");
+      console.log(res);
+    })
   }
 })
