45e1b290ca83f4991f1726bfb6d7e981e111e5fe
zhangkun@boxfish.cn <194264514@qq.com>
2019-08-01 19:51:53 +0800

    command.inc

--- dbdemo/pages/command/command.js
+++ dbdemo/pages/command/command.js
@@ -20,7 +20,8 @@ Page({
     // this.commandAnd();
     // this.commandOr();
     // this.commandUpdateOrSet();
-    this.commandRemove();
+    // this.commandRemove();
+    this.commandInc();
   },
   commandEq: function() {
     db.collection('article').where({
@@ -181,5 +182,31 @@ Page({
     }).then(res =>{
       console.log(res);
     })
+  },
+  // command.inc 更新指令。用于指示字段自增某个值，这是个原子操作，使用这个操作指令而不是先读数据、再加、再写回
+  commandInc:function(){
+    // 添加数据
+    // db.collection('article').add({
+    //   data: {
+    //     title: "重庆保时捷女子吐狂言，知情人：她丈夫是派出所所长，做过很多好事",
+    //     author: " 辣新闻",
+    //     pub_date: new Date(),
+    //     content: "保时捷豪车、香艳美女、两记耳光。7月30日，发生在重庆渝北区龙盛街路口的一起普通的交通违章事故中含有这几个劲爆元素，引起社会强烈反响。更有甚者，事发后有网友曝出当事女子口吐狂言，称自己经常飙车，常闯红灯，打电话就能删除违章记录，从而引发舆论持续发酵。尽管官方至今仍未公布该女子丈夫的情况，但多个权威信源指出，他其实就是派出所所长。",
+    //     tags: ["国际", "科技"],
+    //     read_count:100
+    //   }
+    // }).then(res => {
+    //   console.log("添加成功");
+    //   console.log(res);
+    // });
+    db.collection('article').doc("25c59b425d42d0fa088fd6f315ecf933").update({
+      data:{
+        read_count:_.inc(1)
+      }
+    }).then(
+      res =>{
+        console.log(res);
+      }
+    )
   }
 })
